f11d8b20bd21815e88600729115b6bacfeca06d9
douglas rocha <douglashidekazu15@gmail.com>
2025-11-10 14:14:58 +0900

    added HomeController and owner dashboard logic to redirect users based on their role

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    if current_user.owner?
+      redirect_to owner_dashboard_path
+    else
+      redirect_to appointments_path
+    end
   end
 end
--- app/controllers/owner/dashboard_controller.rb
+++ app/controllers/owner/dashboard_controller.rb
@@ -1,4 +1,14 @@
 class Owner::DashboardController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_owner!
+
   def index
+    @appointments = Appointment.all
+  end
+
+  private
+
+  def require_owner!
+    redirect_to authenticated_root_path unless current_user.owner?
   end
 end
